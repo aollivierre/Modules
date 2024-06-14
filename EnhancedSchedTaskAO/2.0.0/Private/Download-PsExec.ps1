b78cca4974abd2c9139da633739b3f5e86459a94
aollivierre <abdullahollivierre@gmail.com>
2024-06-13 21:01:27 -0700

    improved downloading psexec and ability to exclude modules now

--- EnhancedSchedTaskAO/2.0.0/Private/Download-PsExec.ps1
+++ EnhancedSchedTaskAO/2.0.0/Private/Download-PsExec.ps1
@@ -1,64 +1,57 @@
-
 function Download-PsExec {
     [CmdletBinding()]
     param(
-        # [string]$TargetFolder = "$PSScriptRoot\private"
         [string]$TargetFolder
     )
 
     Begin {
+        # Ensure the target folder exists
+        Ensure-TargetFolderExists -TargetFolder $TargetFolder
+        Write-EnhancedLog -Message "Removing existing PsExec from target folder: $TargetFolder" -Level "INFO" -ForegroundColor ([ConsoleColor]::Cyan)
 
         Remove-ExistingPsExec -TargetFolder $TargetFolder
     }
 
-
-
-    process {
-
-        # Define the URL for PsExec download
-        $url = "https://download.sysinternals.com/files/PSTools.zip"
-    
-        # Ensure the target folder exists
-        if (-Not (Test-Path -Path $TargetFolder)) {
-            New-Item -Path $TargetFolder -ItemType Directory
-        }
-  
-        # Full path for the downloaded file
-        $zipPath = Join-Path -Path $TargetFolder -ChildPath "PSTools.zip"
-  
+    Process {
         try {
+            
+
+            # Define the URL for PsExec download
+            $url = "https://download.sysinternals.com/files/PSTools.zip"
+            # Full path for the downloaded file
+            $zipPath = Join-Path -Path $TargetFolder -ChildPath "PSTools.zip"
+
             # Download the PSTools.zip file containing PsExec
-            Write-EnhancedLog -Message "Downloading PSTools.zip from: $url to: $zipPath"
+            Write-EnhancedLog -Message "Downloading PSTools.zip from: $url to: $zipPath" -Level "INFO" -ForegroundColor ([ConsoleColor]::Cyan)
             Invoke-WebRequest -Uri $url -OutFile $zipPath
-  
+
             # Extract PsExec64.exe from the zip file
+            Write-EnhancedLog -Message "Extracting PSTools.zip to: $TargetFolder\PStools" -Level "INFO" -ForegroundColor ([ConsoleColor]::Cyan)
             Expand-Archive -Path $zipPath -DestinationPath "$TargetFolder\PStools" -Force
-  
+
             # Specific extraction of PsExec64.exe
             $extractedFolderPath = Join-Path -Path $TargetFolder -ChildPath "PSTools"
             $PsExec64Path = Join-Path -Path $extractedFolderPath -ChildPath "PsExec64.exe"
             $finalPath = Join-Path -Path $TargetFolder -ChildPath "PsExec64.exe"
-  
+
             # Move PsExec64.exe to the desired location
             if (Test-Path -Path $PsExec64Path) {
-  
-                Write-EnhancedLog -Message "Moving PSExec64.exe from: $PsExec64Path to: $finalPath"
+                Write-EnhancedLog -Message "Moving PsExec64.exe from: $PsExec64Path to: $finalPath" -Level "INFO" -ForegroundColor ([ConsoleColor]::Cyan)
                 Move-Item -Path $PsExec64Path -Destination $finalPath
-  
+
                 # Remove the downloaded zip file and extracted folder
+                Write-EnhancedLog -Message "Removing downloaded zip file and extracted folder" -Level "INFO" -ForegroundColor ([ConsoleColor]::Cyan)
                 Remove-Item -Path $zipPath -Force
                 Remove-Item -Path $extractedFolderPath -Recurse -Force
-  
-                Write-EnhancedLog -Message "PsExec64.exe has been successfully downloaded and moved to: $finalPath"
+
+                Write-EnhancedLog -Message "PsExec64.exe has been successfully downloaded and moved to: $finalPath" -Level "INFO" -ForegroundColor ([ConsoleColor]::Green)
+            } else {
+                Write-EnhancedLog -Message "PsExec64.exe not found in the extracted files." -Level "ERROR" -ForegroundColor ([ConsoleColor]::Red)
             }
-        }
-        catch {
+        } catch {
             # Handle any errors during the process
-            Write-Error "An error occurred: $_"
+            Write-EnhancedLog -Message "An error occurred during the download or extraction process: $($_.Exception.Message)" -Level "ERROR" -ForegroundColor ([ConsoleColor]::Red)
+            Handle-Error -ErrorRecord $_
         }
     }
-
-
-  
-
 }
