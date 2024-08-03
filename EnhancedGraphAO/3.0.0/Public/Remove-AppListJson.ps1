555c94f4c29e3bbc0fbd84182e1f5ec33687a008
aollivierre <abdullahollivierre@gmail.com>
2024-08-02 22:28:04 -0400

    improving removing app list for Graph app detection

--- EnhancedGraphAO/3.0.0/Public/Remove-AppListJson.ps1
+++ EnhancedGraphAO/3.0.0/Public/Remove-AppListJson.ps1
@@ -1,22 +1,42 @@
 function Remove-AppListJson {
+    [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
-        [string]$jsonPath
+        [string]$JsonPath
     )
 
-    # Check if the file exists
-    if (Test-Path -Path $jsonPath) {
+    begin {
+        Write-EnhancedLog -Message "Starting Remove-AppListJson function" -Level "INFO"
+        Log-Params -Params $PSCmdlet.MyInvocation.BoundParameters
+    }
+
+    process {
         try {
-            # Remove the file
-            Remove-Item -Path $jsonPath -Force
-            Write-EnhancedLog -Message "The applist.json file has been removed successfully."
+            # Check if the file exists
+            if (Test-Path -Path $JsonPath) {
+                # Remove the file
+                $removeParams = @{
+                    Path  = $JsonPath
+                    Force = $true
+                }
+                Remove-Item @removeParams
+                Write-EnhancedLog -Message "The applist.json file has been removed successfully." -Level "INFO"
+            }
+            else {
+                Write-EnhancedLog -Message "The file at path '$JsonPath' does not exist." -Level "WARNING"
+            }
         }
         catch {
-            Write-EnhancedLog -Message "An error occurred while removing the file: $_"
+            Write-EnhancedLog -Message "An error occurred while removing the file: $_" -Level "ERROR"
+            Handle-Error -ErrorRecord $_
             throw $_
         }
     }
-    else {
-        Write-EnhancedLog -Message "The file at path '$jsonPath' does not exist."
+
+    end {
+        Write-EnhancedLog -Message "Remove-AppListJson function execution completed." -Level "INFO"
     }
 }
+
+# Example usage
+# Remove-AppListJson -JsonPath "C:\Path\To\Your\applist.json"
